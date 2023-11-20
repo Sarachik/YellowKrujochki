1be9e7d8774ab790e74c783da49ece4abc585edd
Sarachik <mirdor78@inbox.ru>
2023-11-20 17:14:07 +0300

    Добавил код

--- main.py
+++ main.py
@@ -1,86 +1,41 @@
 import sys
-import random as rr
-import io
-from PyQt5 import uic
-from PyQt5.QtCore import Qt, QPoint
+from random import randint
+
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+from UI import Ui_MainWindow
 
 
-class Suprematism(QWidget):
+class Suprematism(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Ui.ui', self)
-        super().__init__()
-        self.setMouseTracking(True)
-        self.flag = False
+        self.setupUi(self)
         self.qp = QPainter()
-        self.x = 0
-        self.y = 0
-
-    def mousePressEvent(self, event):
-        self.x = event.x()
-        self.y = event.y()
-        if event.button() == Qt.LeftButton:
-            self.figure = 1
-        elif event.button() == Qt.RightButton:
-            self.figure = 1
-        self.drawf()
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
 
     def paintEvent(self, event):
-        if self.flag:
-            # Создаем объект QPainter для рисования
+        if self.do_paint:
             qp = QPainter()
-            # Начинаем процесс рисования
             qp.begin(self)
-            self.draw_rectangle(qp)
-            # Завершаем рисование
+            self.draw(qp)
             qp.end()
 
-    def create_color(self):
-        r = 255
-        g = 255
-        b = 0
-        return r, g, b
-
-    def draw_rectangle(self, qp):
-        n = rr.randint(1, 400)
-        if self.flag and self.figure == 2:
-            x1 = self.x - (n // 2)
-            y1 = self.y - (n // 2)
-            x2 = self.x + (n // 2)
-            y2 = self.y + (n // 2)
-            color = self.create_color()
-            print(color)
-            qp.setBrush(QColor(*color))
-            qp.drawRect(x1, y1, n, n)
-        elif self.flag and self.figure == 1:
-            x1 = self.x - (n // 2)
-            y1 = self.y - (n // 2)
-            color = self.create_color()
-            qp.setBrush(QColor(*color))
-            qp.drawEllipse(x1, y1, n, n)
-        elif self.flag and self.figure == 3:
-            x = self.x
-            y = self.y
-            coordinats = [QPoint(x, y - n), QPoint(int(x + (n ** 2 - (n // 2) ** 2) ** 0.5), y + n // 2),
-                          QPoint(int(x - (n ** 2 - (n // 2) ** 2) ** 0.5), y + n // 2)]
-            color = self.create_color()
-            qp.setBrush(QColor(*color))
-            qp.drawPolygon(coordinats)
-
-    def drawf(self):
-        self.flag = True
+    def paint(self):
+        self.do_paint = True
+        self.make_ball()
         self.update()
 
-    def mouseMoveEvent(self, event):
-        self.x = event.x()
-        self.y = event.y()
+    def make_ball(self):
+        self.bc = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
+        self.bs = randint(0, min(self.size().width(), self.size().height()))
+        self.bx = randint(0, self.size().width() - self.bs if self.size().width() >= self.bs else 0)
+        self.by = randint(0, self.size().height() - self.bs if self.size().height() >= self.bs else 0)
 
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Space:
-            self.figure = 1
-            self.drawf()
+    def draw(self, qp):
+        qp.setBrush(self.bc)
+        qp.drawEllipse(self.bx * (self.bx > 0), self.by * (self.by > 0), self.bs, self.bs)
 
 
 if __name__ == '__main__':
